80e12463cf964611dc7396656412c12467bec0f0
Logesh <Logesh@ops>
2025-06-03 20:21:56 +0530

    VPC creation completed

--- Git hub/Terraform/scripts/main.tf
+++ Git hub/Terraform/scripts/main.tf
@@ -1,21 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.0.0-beta2"
     }
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.37.0"
     }
   }
 }
+
+# VPC creation region
 
 provider "aws" {
   # Configuration options
   region = "us-east-1"
 }
 
+# VPC Name
 resource "aws_vpc" "VPC" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -25,6 +28,9 @@ resource "aws_vpc" "VPC" {
   }
 }
 
+# Subnet creation
+
+# Public Subnet
 resource "aws_subnet" "Public-Subnet" {
   vpc_id     = aws_vpc.VPC.id
   cidr_block = "10.0.1.0/24"
@@ -34,6 +40,7 @@ resource "aws_subnet" "Public-Subnet" {
   }
 }
 
+# Private subnet
 resource "aws_subnet" "Private-Subnet" {
   vpc_id     = aws_vpc.VPC.id
   cidr_block = "10.0.2.0/24"
@@ -43,6 +50,9 @@ resource "aws_subnet" "Private-Subnet" {
   }
 }
 
+# Route Table
+
+# public route table 
 resource "aws_route_table" "Public-Route-Table" {
   vpc_id = aws_vpc.VPC.id
 
@@ -53,6 +63,7 @@ resource "aws_route_table" "Public-Route-Table" {
   }
 }
 
+# Private route table
 resource "aws_route_table" "Private-Route-Table" {
   vpc_id = aws_vpc.VPC.id
 
@@ -63,7 +74,7 @@ resource "aws_route_table" "Private-Route-Table" {
   }
 }
 
-
+# Internet gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.VPC.id
 
@@ -72,11 +83,13 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+# Elastic IP
 resource "aws_eip" "eip" {
   #instance = aws_instance.web.id
-  domain   = "vpc"
+  domain = "vpc"
 }
 
+# Nat Gateway
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.Public-Subnet.id
@@ -90,27 +103,28 @@ resource "aws_nat_gateway" "nat" {
   # depends_on = [aws_internet_gateway.example]
 }
 
-resource "aws_route_table_association" "a" {
+# Route table association
+
+resource "aws_route_table_association" "Public-route-table" {
   subnet_id      = aws_subnet.Public-Subnet.id
   route_table_id = aws_route_table.Public-Route-Table.id
 }
 
 
-resource "aws_route_table_association" "b" {
+resource "aws_route_table_association" "Private-route-table" {
   subnet_id      = aws_subnet.Private-Subnet.id
   route_table_id = aws_route_table.Private-Route-Table.id
 }
 
 
 resource "aws_route" "r" {
-  route_table_id              = aws_route_table.Public-Route-Table.id
+  route_table_id         = aws_route_table.Public-Route-Table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id      = aws_internet_gateway.gw.id
+  gateway_id             = aws_internet_gateway.gw.id
 }
 
-
 resource "aws_route" "r1" {
-  route_table_id              = aws_route_table.Private-Route-Table.id
+  route_table_id         = aws_route_table.Private-Route-Table.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id     # = aws_nat_gateway.nat.id
+  nat_gateway_id         = aws_nat_gateway.nat.id # = aws_nat_gateway.nat.id
 }
