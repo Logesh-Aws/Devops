cf0efd689b990054a8ec92f69a037a7ab40ac672
Logesh <Logesh@ops>
2025-06-11 19:46:44 +0530

    state file moved to s3

--- Git hub/Terraform/scripts/provider.tf
+++ Git hub/Terraform/scripts/provider.tf
@@ -2,3 +2,11 @@ provider "aws" {
   region  = "ap-south-1"       # Change to your desired AWS region
   profile = "default"         # Optional: if using a named profile from ~/.aws/credentials
 }
+
+terraform {
+  backend "s3" {
+    bucket = "bucket.00-test"
+    key    = "Terraform/statefile"
+    region = "ap-south-1"
+  }
+}
